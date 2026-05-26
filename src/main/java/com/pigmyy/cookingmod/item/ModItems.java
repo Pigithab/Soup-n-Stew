b240326a04f4bd5334cbe3bac0d78eeaf8009acb
Pigithab <dimitrisvrettos36@gmail.com>
2026-05-26 06:12:57 +0300

    Stew/Soup now stacks at one
    Oops

--- src/main/java/com/pigmyy/cookingmod/item/ModItems.java
+++ src/main/java/com/pigmyy/cookingmod/item/ModItems.java
@@ -11,19 +11,19 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CookingMod.MOD_ID);
 
     // Register soup/stew items and give them food properties
-    public static final RegistryObject<Item> FISH_STEW = ITEMS.register("fish_stew", () -> new Item(new Item.Properties().food(ModFoodProperties.FISH_STEW)));
+    public static final RegistryObject<Item> FISH_STEW = ITEMS.register("fish_stew", () -> new Item(new Item.Properties().stacksTo(1).food(ModFoodProperties.FISH_STEW)));
 
-    public static final RegistryObject<Item> VEGETABLE_STEW = ITEMS.register("vegetable_stew", () -> new Item(new Item.Properties().food(ModFoodProperties.VEGETABLE_STEW)));
+    public static final RegistryObject<Item> VEGETABLE_STEW = ITEMS.register("vegetable_stew", () -> new Item(new Item.Properties().stacksTo(1).food(ModFoodProperties.VEGETABLE_STEW)));
 
-    public static final RegistryObject<Item> ROTTEN_STEW = ITEMS.register("rotten_stew", () -> new Item(new Item.Properties().food(ModFoodProperties.ROTTEN_STEW)));
+    public static final RegistryObject<Item> ROTTEN_STEW = ITEMS.register("rotten_stew", () -> new Item(new Item.Properties().stacksTo(1).food(ModFoodProperties.ROTTEN_STEW)));
 
-    public static final RegistryObject<Item> PUMPKIN_SOUP = ITEMS.register("pumpkin_soup", () -> new Item(new Item.Properties().food(ModFoodProperties.PUMPKIN_SOUP)));
+    public static final RegistryObject<Item> PUMPKIN_SOUP = ITEMS.register("pumpkin_soup", () -> new Item(new Item.Properties().stacksTo(1).food(ModFoodProperties.PUMPKIN_SOUP)));
 
-    public static final RegistryObject<Item> PORK_STEW = ITEMS.register("pork_stew", () -> new Item(new Item.Properties().food(ModFoodProperties.PORK_STEW)));
+    public static final RegistryObject<Item> PORK_STEW = ITEMS.register("pork_stew", () -> new Item(new Item.Properties().stacksTo(1).food(ModFoodProperties.PORK_STEW)));
 
-    public static final RegistryObject<Item> BEEF_STEW = ITEMS.register("beef_stew", () -> new Item(new Item.Properties().food(ModFoodProperties.BEEF_STEW)));
+    public static final RegistryObject<Item> BEEF_STEW = ITEMS.register("beef_stew", () -> new Item(new Item.Properties().stacksTo(1).food(ModFoodProperties.BEEF_STEW)));
 
-    public static final RegistryObject<Item> CHICKEN_STEW = ITEMS.register("chicken_stew", () -> new Item(new Item.Properties().food(ModFoodProperties.CHICKEN_STEW)));
+    public static final RegistryObject<Item> CHICKEN_STEW = ITEMS.register("chicken_stew", () -> new Item(new Item.Properties().stacksTo(1).food(ModFoodProperties.CHICKEN_STEW)));
 
 
 
